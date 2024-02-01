adbfa39566618d1ad13cb62f47d9ede73e68c32d
Alex Haslam <alexhaslam12@gmail.com>
2024-02-01 17:32:56 +0000

    Add Googly eyes

--- app.py
+++ app.py
@@ -38,6 +38,25 @@ def draw_face(image: Image, face: Face) -> None:
     plot_circle(draw, face.landmarks["left_eye"], radius=10, fill=(255, 0, 0, 0))
 
 
+def add_googly_eyes(image: Image, face: Face, eye_size: float, pupil_size_range: tuple[float, float]) -> None:
+    draw = ImageDraw.Draw(image)
+    radius = 0.5 * float(np.linalg.norm(np.array(face.landmarks["right_eye"]) - np.array(face.landmarks["left_eye"])))
+
+    def plot_googly_eye(eye: list[float]) -> None:
+        pupil_size = eye_size * np.random.uniform(pupil_size_range[0], pupil_size_range[1])
+        plot_circle(draw, eye, radius=eye_size * radius, fill=(255, 255, 255, 0), outline=(0, 0, 0, 0))
+        orientation = np.random.uniform(0, np.pi)
+        plot_circle(
+            draw,
+            np.array(eye) + (eye_size - pupil_size) * radius * np.array([np.sin(orientation), np.cos(orientation)]),
+            radius=pupil_size * radius,
+            fill=(0, 0, 0, 0),
+        )
+
+    plot_googly_eye(face.landmarks["right_eye"])
+    plot_googly_eye(face.landmarks["left_eye"])
+
+
 st.title(":eyes: Goofy eyes")
 
 uploaded_file = st.file_uploader("Upload photo", type=["png", "jpg"])
@@ -45,9 +64,15 @@ if uploaded_file is None:
     st.error("No image uploaded")
     st.stop()
 
+eye_size = st.slider("Eye size", min_value=0.0, max_value=1.0, value=0.5)
+pupil_size_range = st.slider("Pupil size", min_value=0.0, max_value=1.0, value=[0.4, 0.6])
+highlight_faces = st.checkbox("Highlight identified faces")
+
 image = Image.open(uploaded_file)
 
 for face in identify_faces(image):
-    draw_face(image, face)
+    if highlight_faces:
+        draw_face(image, face)
+    add_googly_eyes(image, face, eye_size, pupil_size_range)
 
 st.image(image)
