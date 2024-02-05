96dbcb55ade26a432653964323dbcabcdb0a302d
Alex Haslam <alexhaslam12@gmail.com>
2024-02-05 11:00:16 +0000

    Add type hints to preprocessing

--- retinaface/commons/preprocess.py
+++ retinaface/commons/preprocess.py
@@ -41,21 +41,20 @@ def get_image(img_uri: str | np.ndarray) -> np.ndarray:
     return img
 
 
-def resize_image(img: np.ndarray, scales: list, allow_upscaling: bool) -> tuple:
+def resize_image(img: np.ndarray, target_size: int, max_size: int, allow_upscaling: bool) -> tuple[np.ndarray, float]:
     """
     This function is modified from the following code snippet:
     https://github.com/StanislasBertrand/RetinaFace-tf2/blob/5f68ce8130889384cb8aca937a270cea4ef2d020/retinaface.py#L49-L74
 
     Args:
         img (numpy array): given image
-        scales (list)
+        target_size: int
+        max_size: int
         allow_upscaling (bool)
     Returns
         resized image, im_scale
     """
     img_h, img_w = img.shape[0:2]
-    target_size = scales[0]
-    max_size = scales[1]
 
     if img_w > img_h:
         im_size_min, im_size_max = img_h, img_w
@@ -77,7 +76,7 @@ def resize_image(img: np.ndarray, scales: list, allow_upscaling: bool) -> tuple:
     return img, im_scale
 
 
-def preprocess_image(img: np.ndarray, allow_upscaling: bool) -> tuple:
+def preprocess_image(img: np.ndarray, allow_upscaling: bool) -> tuple[np.ndarray, tuple[int, int], float]:
     """
     This function is modified from the following code snippet:
     https://github.com/StanislasBertrand/RetinaFace-tf2/blob/5f68ce8130889384cb8aca937a270cea4ef2d020/retinaface.py#L76-L96
@@ -90,14 +89,14 @@ def preprocess_image(img: np.ndarray, allow_upscaling: bool) -> tuple:
     pixel_means = np.array([0.0, 0.0, 0.0], dtype=np.float32)
     pixel_stds = np.array([1.0, 1.0, 1.0], dtype=np.float32)
     pixel_scale = float(1.0)
-    scales = [1024, 1980]
 
-    img, im_scale = resize_image(img, scales, allow_upscaling)
+    img, im_scale = resize_image(img, target_size=1024, max_size=1980, allow_upscaling=allow_upscaling)
     img = img.astype(np.float32)
     im_tensor = np.zeros((1, img.shape[0], img.shape[1], img.shape[2]), dtype=np.float32)
+    im_shape = (img.shape[0], img.shape[1])
 
     # Make image scaling + transpose (N,H,W,C) to (N,C,H,W)
     for i in range(3):
         im_tensor[0, :, :, i] = (img[:, :, i] / pixel_scale - pixel_means[i]) / pixel_stds[i]
 
-    return im_tensor, img.shape[0:2], im_scale
+    return im_tensor, im_shape, im_scale
